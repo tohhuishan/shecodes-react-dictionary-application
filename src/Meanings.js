747e342db9f365f69f3e796bb5159cebdf179028
tohhuishan <tohhuishan12@gmail.com>
2023-03-11 07:52:12 +0800

    Solved results display bug

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,24 +1,28 @@
 import React from "react";
 
 export default function Meanings(props) {
-  return (
-    <div className="Meanings">
-      <h2>{props.results.word}</h2>
-      <h3>{props.results.phonetic}</h3>
-      {props.results.meanings.map((meaning, index) => {
-        return (
-          <div className="Meaning" key={index}>
-            <p>{meaning.partOfSpeech}</p>
+  if (props.results) {
+    return (
+      <div className="Meanings">
+        <h2>{props.results.word}</h2>
+        <h3>{props.results.phonetic}</h3>
+        {props.results.meanings.map((meaning, index) => {
+          return (
+            <div className="Meaning" key={index}>
+              <p>{meaning.partOfSpeech}</p>
 
-            <p>{meaning.definition}</p>
+              <p>{meaning.definition}</p>
 
-            <p>{meaning.example}</p>
+              <p>{meaning.example}</p>
 
-            {/* 
+              {/* 
             <p>{meaning.synonyms}</p> */}
-          </div>
-        );
-      })}
-    </div>
-  );
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
